ff3e13f1db3021ab22c6d7dcae12d5c565c0609c
sriniketh7 <srinikethchalla6304@gmail.com>
2026-06-30 22:43:21 +0530

    Initial commit

---
+++ ailoganlzr/src/main/java/com/proj1/ailoganlzr/AiloganlzrApplication.java
@@ -0,0 +1,13 @@
+package com.proj1.ailoganlzr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiloganlzrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiloganlzrApplication.class, args);
+	}
+
+}
