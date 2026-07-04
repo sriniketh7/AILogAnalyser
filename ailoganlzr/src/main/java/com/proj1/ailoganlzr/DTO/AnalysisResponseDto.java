c13472bf85ca844a62db4b1b502be5b01c8f449b
sriniketh7 <srinikethchalla6304@gmail.com>
2026-07-04 23:26:19 +0530

    controller code for a sample request added.

--- ailoganlzr/src/main/java/com/proj1/ailoganlzr/DTO/AnalysisResponseDto.java
+++ ailoganlzr/src/main/java/com/proj1/ailoganlzr/DTO/AnalysisResponseDto.java
@@ -1,7 +1,15 @@
 package com.proj1.ailoganlzr.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnalysisResponseDto {
 
     private Long id;
---
+++ ailoganlzr/src/main/java/com/proj1/ailoganlzr/controller/AnalysisRequestController.java
@@ -0,0 +1,31 @@
+package com.proj1.ailoganlzr.controller;
+
+import com.proj1.ailoganlzr.DTO.AnalysisRequestDto;
+import com.proj1.ailoganlzr.DTO.AnalysisResponseDto;
+import com.proj1.ailoganlzr.service.AnalysisRequestService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/analysis")
+@RequiredArgsConstructor
+public class AnalysisRequestController {
+
+    private final AnalysisRequestService service;
+
+    @PostMapping
+    public ResponseEntity<AnalysisResponseDto> create(
+            @Valid @RequestBody AnalysisRequestDto dto) {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.createAnalysisRequest(dto));
+
+    }
+
+}
